66bb5e3429f6a30a346c2c730591f925a836aa6d
Antoine T <antoine.t5656@gmail.com>
2024-03-14 10:32:20 +0100

    feat : Init front for Users

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -4,6 +4,11 @@ import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
 import About from "../views/About.vue";
 import manageUsers from "../views/ManageUsers.vue";
+import HomeUser from "../views/HomeUser.vue";
+import MyMovies from "../views/MyMovies.vue";
+import MyHistory from "../views/MyHistory.vue";
+import MyAccount from "../views/MyAccount.vue";
+import MyCart from "../views/MyCart.vue";
 import MovieDetailsPage from "../views/MovieDetailsPage.vue";
 
 const routes = [
@@ -33,6 +38,32 @@ const routes = [
         name: 'manageUsers',
         component: manageUsers,
     },
+    {
+        path: '/HomeUser',
+        name: 'HomeUser',
+        component: HomeUser,
+    },
+    {
+        path: '/MyMovies',
+        name: 'MyMovies',
+        component: MyMovies,
+    },
+    {
+        path: '/MyHistory',
+        name: 'MyHistory',
+        component: MyHistory,
+    },
+    {
+        path: '/MyAccount',
+        name: 'MyAccount',
+        component: MyAccount,
+    },
+    {
+        path: '/MyCart',
+        name: 'MyCart',
+        component: MyCart,
+    },
+    // ...autres routes
     {
         path: '/MovieDetailsPage/:movieId',
         name: 'MovieDetailsPage',
---
+++ front/src/views/MyCart.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="mycart">
+    <NavbarUser/>
+    <h1>Mon Panier</h1>
+    <!-- Ici, vous pouvez ajouter le code pour afficher les articles dans le panier -->
+  </div>
+</template>
+
+<script>
+import NavbarUser from "../components/NavbarUser.vue";
+
+export default {
+  name: 'MyCart',
+  components: {NavbarUser},
+  data() {
+    return {
+      // Ici, vous pouvez ajouter les données nécessaires pour votre panier
+    };
+  },
+  methods: {
+    // Ici, vous pouvez ajouter les méthodes nécessaires pour votre panier
+  },
+};
+</script>
+
+<style scoped>
+.mycart {
+  /* Ici, vous pouvez ajouter le style pour votre panier */
+}
+</style>
