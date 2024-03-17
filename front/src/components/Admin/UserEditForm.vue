497f50e4c3a2f240dcde0c3550f3adc46e6c3b11
Antoine T <antoine.t5656@gmail.com>
2024-03-17 19:33:51 +0100

    feat : manage user no working

---
+++ front/src/components/Admin/UserEditForm.vue
@@ -0,0 +1,161 @@
+<template>
+  <div class="edit-user-form">
+    <h2>Édition de l'utilisateur : {{ user.last_name }} {{ user.first_name }}</h2>
+    <!-- Formulaire d'édition d'utilisateur -->
+    <form @submit.prevent="submitEdit">
+      <!-- Ajoutez tous les champs nécessaires -->
+      <label for="email">Email:</label>
+      <input type="email" id="email" v-model="editFormData.email">
+
+      <label for="role">Rôle:</label>
+      <select id="role" v-model="editFormData.role">
+        <option value="user">Utilisateur</option>
+        <option value="admin">Admin</option>
+        <!-- Ajoutez d'autres rôles si nécessaire -->
+      </select>
+
+      <!-- Champs existants -->
+      <label for="first-name">Prénom:</label>
+      <input type="text" id="first-name" v-model="editFormData.first_name">
+
+      <label for="last-name">Nom de famille:</label>
+      <input type="text" id="last-name" v-model="editFormData.last_name">
+
+
+      <button type="submit">Sauvegarder les modifications</button>
+
+
+      <!-- Bouton pour activer le formulaire de changement de mot de passe -->
+      <button type="button" v-if="!showPasswordChangeForm" @click="showPasswordChangeForm = true">Modifier le mot de passe</button>
+
+      <!-- Bouton pour fermer le formulaire de changement de mot de passe -->
+      <button type="button" v-if="showPasswordChangeForm" @click="showPasswordChangeForm = false">Annuler le  changement de mot de passe</button>
+    </form>
+
+    <!-- Formulaire de changement de mot de passe -->
+    <div v-if="showPasswordChangeForm">
+      <form @submit.prevent="submitPasswordChange">
+        <label for="new-password">Nouveau mot de passe:</label>
+        <input type="password" id="new-password" v-model="newPassword">
+
+        <label for="confirm-password">Confirmer le mot de passe:</label>
+        <input type="password" id="confirm-password" v-model="confirmPassword">
+
+        <button type="submit">Changer le mot de passe</button>
+      </form>
+    </div>
+
+    <button @click="$emit('close')">Annuler</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  props: {
+    user: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      editFormData: {...this.user},
+      showPasswordChangeForm: false,
+      newPassword: '',
+      confirmPassword: '',
+    };
+  },computed: {
+    isPasswordInvalid() {
+      // Vous pouvez ajouter d'autres conditions de validation ici
+      return this.newPassword !== this.confirmPassword || !this.newPassword || !this.confirmPassword;
+
+    }
+  },
+
+  methods: {
+    async submitEdit() {
+      // Logique de mise à jour de l'utilisateur
+      console.log('Submitted', this.editFormData);
+      // Mise à jour via API...
+      this.$emit('close');
+    },
+    async submitPasswordChange() {
+      if (this.isPasswordInvalid) {
+        alert("Le mot de passe n'est pas valide.");
+        return;
+      }
+
+      try {
+        // Utilisez le bon endpoint pour la modification du mot de passe
+        const response = await axios.post(`http://localhost:3000/user/password/${this.user.id}`, {
+          user_id: this.user.id,
+          new_password: this.newPassword,
+          // Vous pourriez avoir besoin d'envoyer l'ancien mot de passe également, selon votre API
+        });
+        console.log('Changement de mot de passe réussi', response.data);
+        this.showPasswordChangeForm = false; // Cacher le formulaire après la réussite
+      } catch (error) {
+        console.error('Erreur lors du changement de mot de passe', error);
+        this.errorMessage = 'Une erreur est survenue lors du changement de mot de passe.';
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.edit-user-form {
+  background: #f4f4f4;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  max-width: 500px;
+  margin: auto;
+}
+
+.edit-user-form h2 {
+  margin-bottom: 20px;
+  color: #333;
+}
+
+.edit-user-form form {
+  display: grid;
+  grid-gap: 10px;
+  margin-bottom: 20px;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 5px;
+}
+
+input[type="text"],
+input[type="email"],
+input[type="password"],
+select {
+  width: 100%;
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+button {
+  cursor: pointer;
+  padding: 10px 15px;
+  border: none;
+  border-radius: 4px;
+  background-color: #007bff;
+  color: white;
+  transition: background-color 0.3s;
+}
+
+button[type="button"] {
+  background-color: #f44336;
+}
+
+button[type="submit"]:disabled {
+  background-color: #ccc;
+  cursor: not-allowed;
+}
+</style>
--- front/src/views/Admin/ManageUsers.vue
+++ front/src/views/Admin/ManageUsers.vue
@@ -1,45 +1,50 @@
 <template>
-  <NavbarAdmin/>
-  <div class="main-content">
-    <h1>Gestion des utilisateurs</h1>
-    <button class="create-user-button" @click="createUser">Créer un utilisateur</button>
-    <div class="content-wrapper">
-      <div class="user-list">
+  <div class="main-body">
+    <NavbarAdmin/>
+    <div class="main-content">
+      <h1>Gestion des utilisateurs</h1>
+      <button class="create-user-button" @click="createUser">Créer un utilisateur</button>
+      <div class="content-wrapper">
+        <div class="user-list">
 
-        <!-- Boucle sur les rôles prédéfinis -->
-        <div v-for="role in roles" :key="role" class="role-section">
-          <h1 class="role-title">{{ role }}</h1>
-          <!-- Boucle sur les utilisateurs du rôle actuel -->
-          <div class="user-card" @click="selectUser(user)"
-               v-for="user in usersFilteredByRole(role)"
-               :key="user.id">
-            <div class="user-info">
-              <h3>{{ user.last_name }} {{ user.first_name }}</h3>
-              <p><strong>Email :</strong> {{ user.email }}</p>
-              <p><strong>Activé :</strong> {{ user.activated ? 'Oui' : 'Non' }}</p>
-              <p><strong>Rôle :</strong> {{ user.role }}</p>
-              <div class="user-actions">
-                <button @click="selectUser(user)">Détails</button>
+          <!-- Boucle sur les rôles prédéfinis -->
+          <div v-for="role in roles" :key="role" class="role-section">
+            <h1 class="role-title">{{ role }}</h1>
+            <!-- Boucle sur les utilisateurs du rôle actuel -->
+            <div class="user-card" @click="selectUser(user)"
+                 v-for="user in usersFilteredByRole(role)"
+                 :key="user.id">
+              <div class="user-info" @click="selectUser(user, 'details')">
+                <h3>{{ user.last_name }} {{ user.first_name }}</h3>
+                <p><strong> Email : </strong> {{ user.email }}</p>
+                <p><strong> Activé : </strong> {{ user.activated ? 'Oui' : 'Non' }}</p>
+                <p><strong> Rôle : </strong> {{ user.role }}</p>
+                <div class="user-actions">
+
+                  <!-- Boutons d'actions, notez l'utilisation de stop pour empêcher la propagation de l'événement au conteneur parent -->
+                  <button @click.stop="selectUser(user, 'edit')"> Modifier </button>
+                  <button @click.stop="deleteUser(user)"> Supprimer </button>
+                  <button @click.stop="selectUser(user, 'rentedMovies')"> Films loués </button>
+                  <button @click.stop="selectUser(user, 'purchasedMovies')"> Films achetés </button>
+                </div>
               </div>
             </div>
           </div>
+
         </div>
 
-      </div>
-      <div class="user-details user-card">
-        <!-- Les composants de détail / modification sont affichés ici -->
-        <!-- Affichage conditionnel basé sur l'utilisateur sélectionné -->
-        <div v-if="selectedUser">
-          <h3>Détails de {{ selectedUser.last_name }} {{ selectedUser.first_name }}</h3>
-          <p><strong>Email :</strong> {{ selectedUser.email }}</p>
-          <p><strong>Activé :</strong> {{ selectedUser.activated ? 'Oui' : 'Non' }}</p>
-          <p><strong>Rôle :</strong> {{ selectedUser.role }}</p>
-          <!-- Boutons d'actions -->
-          <button @click="editUser(selectedUser)">Modifier</button>
-          <button @click="deleteUser(selectedUser)">Supprimer</button>
-          <button @click="showRentedMovies(selectedUser)">Afficher les films loués</button>
-          <button @click="showPurchasedMovies(selectedUser)">Afficher les films achetés</button>
+        <div class="user-details">
+          <div class="user-card">
 
+            <!-- Les composants de détail / modification sont affichés ici -->
+            <!-- Affichage conditionnel basé sur l'utilisateur sélectionné -->
+            <div v-if="selectedUser">
+              <component v-if="currentAction" :is="components[currentAction]" :user="selectedUser"
+                         @close="currentAction = null"></component>
+
+
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -51,27 +56,38 @@
 import NavbarAdmin from '../../components/Admin/NavbarAdmin.vue';
 import Footer from '../../components/Core/Footer.vue';
 import axios from 'axios';
+import UserEditForm from "../../components/Admin/UserEditForm.vue";
 
 export default {
   components: {
     NavbarAdmin,
-    Footer
+    Footer,
+    UserEditForm,
+
   },
+
 
   data() {
     return {
       users: [],
       selectedUser: null,
       roles: ['admin', 'user'], // Rôles prédéfinis
+      currentAction: null,
     };
   },
   computed: {
-    uniqueRoles() {
-      if (!this.users.length) return []; // S'assure que users n'est pas vide
-      return [...new Set(this.users.map(user => user.role))];
-    }
+    components() {
+      return {
+        // 'details': UserDetailsComponent,
+        'edit': UserEditForm,
+        // 'delete': UserDeleteComponent,
+        // 'rentedMovies': UserRentedMoviesComponent,
+        // 'purchasedMovies': UserPurchasedMoviesComponent,
+      };
+    },
   },
   methods: {
+
     async fetchUsers() {
       try {
         const response = await axios.get('http://localhost:3000/users');
@@ -83,14 +99,17 @@ export default {
     usersFilteredByRole(role) {
       return this.users.filter(user => user.role === role);
     },
-    selectUser(user) {
+    selectUser(user, action) {
       this.selectedUser = user;
+      this.currentAction = action;
     },
-    editUser(user) {
-      // Logique pour modifier l'utilisateur
-    },
-    deleteUser(user) {
-      // Logique pour supprimer l'utilisateur
+    async deleteUser(user) {
+      try {
+        await axios.delete(`http://localhost:3000/users/${user.id}`);
+        this.users = this.users.filter(u => u.id !== user.id);
+      } catch (error) {
+        console.error("Erreur lors de la suppression de l'utilisateur:", error);
+      }
     },
     showRentedMovies(user) {
       // Logique pour afficher les films loués par l'utilisateur
@@ -107,10 +126,18 @@ export default {
 </script>
 
 <style scoped>
+/* Styles pour votre page */
+
+.main-body {
+  max-height: 100vh;
+  overflow-y: hidden;
+}
 .main-content {
   display: flex;
   flex-direction: column;
   align-items: center;
+  max-height: 90vh;
+  overflow-y: hidden;
 }
 
 .content-wrapper {
@@ -121,7 +148,7 @@ export default {
 .user-list {
   flex: 1;
   overflow-y: auto; /* Barre de défilement pour la liste des utilisateurs */
-  height: 80vh; /* Hauteur fixe pour activer le défilement */
+  height: 75vh; /* Hauteur fixe pour activer le défilement */
   padding: 20px;
 }
 
@@ -139,8 +166,16 @@ export default {
   margin: 10px 0;
   padding: 15px;
   border-radius: 5px;
-  cursor: pointer;
 }
 
+.user-actions button {
+  /* Styles pour vos boutons */
+  margin-top: 10px; /* Ajouter un peu d'espace au-dessus des boutons */
+  width: 100%; /* Assurez-vous que les boutons occupent toute la largeur */
+}
+
+body {
+  overflow: hidden;
+}
 
 </style>
