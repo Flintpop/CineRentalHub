5908f6090da0dc5c067df3aca1e720c199b02a8a
Antoine T <antoine.t5656@gmail.com>
2024-03-18 10:45:10 +0100

    fix : name bdd in node

--- back/node/app.js
+++ back/node/app.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-    host: 'localhost',
+    host: 'mariadb',
     port: '3306',
     user: 'root',
     password: 'root',
