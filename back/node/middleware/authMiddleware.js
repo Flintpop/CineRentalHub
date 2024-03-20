989631334ac47c325bc2a6ee2b1e9a2037b201ed
Flintpop <tanguy.legoff2@gmail.com>
2024-03-20 21:53:41 +0100

    fix: Wrong route to post commments

--- back/node/middleware/authMiddleware.js
+++ back/node/middleware/authMiddleware.js
@@ -33,7 +33,7 @@ const verifyJWTAndRole = (req, res, next) => {
 
 
         // Pb d'id, je ne sais pas comment récupérer l'id du commentaire à modifier
-        {path: '/comments', methods: ['POST'], roles: ['user'], selfOnly: false},
+        {path: '/comments/[^/]+/?', methods: ['POST'], roles: ['user'], selfOnly: false},
         {path: '/comments/manage/[^/]+/?', methods: ['PUT', 'DELETE', 'GET'], roles: ['user'], selfOnly: false},
         {path: '/comments/image', methods: ['POST'], roles: ['user'], selfOnly: false},
         {path: '/comments/image/[^/]+/?', methods: ['DELETE'], roles: ['user'], selfOnly: false},
