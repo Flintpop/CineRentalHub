fbae4aeb73bc65ec0053adc0d104f43e6db44824
Flintpop <tanguy.legoff2@gmail.com>
2024-03-21 10:35:55 +0100

    feat: Deletion of stats, with icon

--- front/src/main.js
+++ front/src/main.js
@@ -9,7 +9,13 @@ const app = createApp(App);
 // Utiliser le plugin 'utils' avec Vue 3.
 app.use(utils);
 app.use(router);
+// FontAwesome Import
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faTrashAlt);
 app.config.globalProperties.$configureAxios();
-
+app.component('font-awesome-icon', FontAwesomeIcon);
 app.mount('#app');
